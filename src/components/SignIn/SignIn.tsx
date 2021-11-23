2bfd4eaaa2157a6e3349cf6fa8a188d1d6fd8cbb
kudarejoice <cameoverarn@gmail.com>
2021-11-23 14:46:37 -0600

    commit

---
+++ src/components/SignIn/SignIn.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { DataTable } from '../../components';
+
+export const SignIn = () => {
+    return (
+        <div>
+            <h1>Sign In Here</h1>
+            <DataTable />
+        </div>
+    )
+}
