500c970460ac8901e0b529ba4331266a192b4618
kudarejoice <cameoverarn@gmail.com>
2021-11-24 07:49:48 -0600

    day2

--- src/components/DataTable/DataTable.tsx
+++ src/components/DataTable/DataTable.tsx
@@ -4,35 +4,35 @@ import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 
 const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', width: 70 },
-    { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },
+    { field: 'make', headerName: 'Make', width: 130 },
+    { field: 'model', headerName: 'Model', width: 130 },
     {
-      field: 'age',
-      headerName: 'Age',
+      field: 'year',
+      headerName: 'Year',
       type: 'number',
       width: 90,
     },
     {
-      field: 'fullName',
-      headerName: 'Full name',
+      field: 'makeandmodel',
+      headerName: 'Cars',
       description: 'This column has a value getter and is not sortable.',
       sortable: false,
       width: 160,
-      //valueGetter: (params: GridValueGetterParams) =>
-      //  `${params.getValue('firstName') || ''} ${params.getValue('lastName') || ''}`,
+   // valueGetter: (params: GridValueGetterParams) =>
+       // `${params.getValue('model') || ''} ${params.getValue('make') || ''}`,
     },
   ];
 
   const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-    { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-    { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-    { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-    { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-    { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-    { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-    { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-    { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+    { id: 1, make: 'Audi', model: 'R8', year: '2022' },
+    { id: 2, make: 'Chevy', model: 'Camaro ZL1', year: '2017' },
+    { id: 3, make: 'Dodge', model: 'Viper', year: '2012' },
+    { id: 4, make: 'Dodge', model: 'Ram', year: '2019' },
+    { id: 5, make: 'Toyota', model: 'Tundra', year: '2020' },
+    { id: 6, make: 'Plymouth', model: 'Baracuda', year: '1970' },
+    { id: 7, make: 'Mercedes', model: 'Benz', year: '1955' },
+    { id: 8, make: 'Volkswagen', model: 'Beetle', year: '1967' },
+    { id: 9, make: 'SSC', model: 'Tuatara', year: '2021' },
   ];
 
 export const DataTable = () =>{
